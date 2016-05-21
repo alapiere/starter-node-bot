9840ea7ffd5a189a2f7b3c95a4d5090fe6f34d5a
Arnaud Lapiere <lapiere@gmail.com>
2016-05-21 03:34:25 +0200

    test

--- index.js
+++ index.js
@@ -76,27 +76,10 @@ controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   var wit = witbot.process(message.text, bot, message)
    
-   wit.hears('hello', 0.53, function (bot, message, outcome) {
-    bot.startConversation(message, function (_, convo) {
-      convo.say('Hello!')
-      convo.ask('How are you?', function (response, convo) {
-        witbot.process(response.text)
-          .hears('good', 0.5, function (outcome) {
-            convo.say('I am so glad to hear it!')
-            convo.next()
-          })
-          .hears('bad', 0.5, function (outcome) {
-            convo.say('I\'m sorry, that is terrible')
-            convo.next()
-          })
-          .otherwise(function (outcome) {
-            convo.say('I\'m cofused')
-            convo.repeat()
-            convo.next()
-          })
-      })
-    })
-  })  
+   wit.hears('congés', 0.53, function (bot, message, outcome) {
+
+      convo.say('leave !')
+   })
 })
 
 
